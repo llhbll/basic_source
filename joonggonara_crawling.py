65bdb9d9cfca4598d20592dc2ff18a5d5b59fe6e
llhbll <llhbll00@gmail.com>
2020-05-25 12:01:32 +0900

    중고나라 리스트에서 클릭하여 상세정보를 가져오기 webdriver를 2개 운영하여 coding 엑셀에 저장하고 하이퍼링크 연결
    자동로긴하여(자동입력방지 뚫는 소스적용) 네이이버 로긴해야만 상세페이지 정보 가져오는거 구현

--- joonggonara_crawling.py
+++ joonggonara_crawling.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 import time
 from openpyxl import Workbook, load_workbook
+import pyperclip
+from selenium.webdriver.common.keys import Keys
 
 wb  = Workbook()
 sheet1 = wb.active
@@ -13,18 +15,53 @@ sheet1.cell(row=1, column=3).value = '작성자'
 sheet1.cell(row=1, column=4).value = '날짜'
 sheet1.cell(row=1, column=5).value = '가격'
 
-
+def uf_get_idpw(siteName):
+    f = open("./pw.txt", 'r')
+    for data in f.readlines():
+        site = data.split(':')[0]
+        id = data.split(':')[1]
+        pw = data.split(':')[2].strip('\n')
+        pw = 'lch' + pw #pw file이 노출되었을때를 대비하여 앞 3글자는 여기서
+        if site == siteName:
+            return id, pw
+keyword = input("키워드 입력 : ")
+#
 driver = webdriver.Chrome('./chromedriver.exe')
 driver2 = webdriver.Chrome('./chromedriver.exe')
+#
+#
+# 자동입력 방지를 뚫기위해 pyperclip을 사용 로긴
+user_id, user_pw = uf_get_idpw('naver')
+naverlogin_url = 'https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com'
+driver.get(naverlogin_url)
+time.sleep(1)
+naver_id_input = driver.find_element_by_css_selector('#id')
+naver_id_input.clear()
+time.sleep(1)
 
-joonggonara_url = 'https://cafe.naver.com/joonggonara.cafe?iframe_url=/ArticleList.nhn%3Fsearch.clubid=10050146%26search.boardtype=L%26viewType=pc'
+naver_id_input.click()
+pyperclip.copy(user_id)
+naver_id_input.send_keys(Keys.CONTROL, 'v')
+time.sleep(1)
+
+naver_pw_input = driver.find_element_by_css_selector('#pw')
+naver_pw_input.click()
+pyperclip.copy(user_pw)
+naver_pw_input.send_keys(Keys.CONTROL, 'v')
+time.sleep(1)
+
+search_button = driver.find_element_by_css_selector("#log\.login")
+search_button.click()
+time.sleep(2)
+
+joonggonara_url = 'https://cafe.naver.com/joonggonara'
+# joonggonara_url = 'https://cafe.naver.com/joonggonara.cafe?iframe_url=/ArticleList.nhn%3Fsearch.clubid=10050146%26search.boardtype=L%26viewType=pc'
 
 driver.get(joonggonara_url)
 
-# keyword = input("키워드 입력 : ")
 search_input = driver.find_element_by_css_selector('input#topLayerQueryInput')
-search_input.send_keys('갤럭시 탭')
-# search_input.send_keys(keyword)
+# search_input.send_keys('갤럭시 탭')
+search_input.send_keys(keyword)
 search_button = driver.find_element_by_css_selector("form[name='frmBoardSearch'] > button")
 search_button.click()
 
@@ -44,7 +81,7 @@ for page in range(1, 2):
 
 
         driver2.get(link)
-        time.sleep(1)
+        time.sleep(3)
         driver2.switch_to.frame("cafe_main")
         try:
            cost = driver2.find_element_by_css_selector('span.cost').text
@@ -66,7 +103,7 @@ for page in range(1, 2):
         next = str(page + 1)
         driver.find_element_by_link_text(next).click()
 
-wb.save("./test4.xlsx")
+wb.save(keyword + ".xlsx")
 
         #item.find_element_by_css_selector('a').click()  #가격을 구하기 위해서
 
