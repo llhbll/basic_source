8c2c584c766bdcefc3a6c3ba5fbd012939eaf617
llhbll <llhbll00@gmail.com>
2020-05-26 09:57:50 +0900

    중고나라 리스트에서 클릭하여 상세정보를 가져오기 webdriver를 2개 운영하여 coding 엑셀에 저장하고 하이퍼링크 연결
    자동로긴하여(자동입력방지 뚫는 소스적용) 네이이버 로긴해야만 상세페이지 정보 가져오는거 구현

--- basic.py
+++ basic.py
@@ -41,3 +41,31 @@
 # # 가끔 실행오류가 나는 경우가 생기고, send key에서 첫글자는 전송이 안되는 문제 있음.
 # # 번역 버튼 클릭
 # driver.find_element_by_css_selector('#btnTranslate').click()
+
+# 999 폴더 file 핸들링
+# import os
+#
+# def search(dirname):
+#     try:
+#         filenames = os.listdir(dirname)
+#         for filename in filenames:
+#             full_file_name = os.path.join(dirname, filename)
+#             if os.path.isdir(full_file_name):
+#                 search(full_file_name)
+#             else:
+#                 ext = os.path.splitext(full_file_name)[-1]
+#                 if ext == '.py':
+#                     print(full_file_name)
+#     except PermissionError:
+#         pass
+#
+# search("c:/")
+
+# 999 폴더 file 핸들링2
+# import os
+#
+# for(path, dir, filenames) in os.walk("c:/"):
+#     for filename in filenames:
+#         ext = os.path.splitext(filename)[-1]
+#         if(ext == ".py"):
+#             print("%s%s" %(path, filename))
